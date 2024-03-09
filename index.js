f53a57c4ecdd0309b0317a2d0726b1aaf37e2e53
John Lowther <jw.lowther@hotmail.co.uk>
2024-03-09 00:40:51 +0000

    Add in javascript file and mail opening

---
+++ index.js
@@ -0,0 +1,7 @@
+function sendEmail() {
+  // Email encoded to obfuscate email
+  const email = atob('c2FuZHJhX2F0X3BqY0B5YWhvby5jb20=');
+  const subject = `subject=${encodeURIComponent("Cancer Diaries Enquiry")}`;
+  const message = `body=${encodeURIComponent("")}`;
+  window.location.href = `mailto:${email}?${subject}&${message}`;
+}
